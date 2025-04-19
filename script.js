955dd062a067ccafff7f6b5b8fef48f54fbd151c
lateef76 <Lateefabiodun76@gmail.com>
2025-04-19 18:07:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder for any future JavaScript functionality
+document.querySelectorAll('.plan button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('You clicked on Invest Now!');
+    });
+});
